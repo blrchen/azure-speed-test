9d394ac310fbe6678d7b5cfd9253ae72dde2c641
blchen <blrchen@hotmail.com>
2016-06-04 23:21:11 +0800

    Clean up useless deploy scripts

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,3 @@
 @echo off
 REM msbuild AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeedeastasia.pubxml /p:Configuration=Release
 msbuild AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeedwestus.pubxml /p:Configuration=Release
-msbuild AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeedchinaeast.pubxml /p:Configuration=Release

29c4c60e656c93501766f2fd8b2dba1fa614a3d9
blchen <blrchen@hotmail.com>
2016-10-11 11:18:10 +0800

    Update elist config to scan all app js files

--- AzureSpeed.Web.App/gulpfile.js
+++ AzureSpeed.Web.App/gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('lint', () => {
     // So, it's best to have gulp ignore the directory as well. 
     // Also, Be sure to return the stream from the task; 
     // Otherwise, the task may end before the stream has finished. 
-    return gulp.src(['wwwroot/js/azurespeed/*.js', '!node_modules/**'])
+    return gulp.src(['wwwroot/js/azurespeed/**/*.js', '!node_modules/**'])
         // eslint() attaches the lint output to the "eslint" property 
         // of the file object so it can be used by other modules. 
         .pipe(eslint())

306efb84dc93f7540cc585141140fb695f88e5b7
blchen <blrchen@hotmail.com>
2017-05-04 20:58:12 +0800

    Deploy to staging enviroment by default

--- scripts/DeployWebApp.cmd
+++ scripts/DeployWebApp.cmd
@@ -1,2 +1,2 @@
 @echo off
-msbuild ..\AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeedwestus.pubxml /p:Configuration=Release
+msbuild ..\AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeed-wus-staging.pubxml /p:Configuration=Release

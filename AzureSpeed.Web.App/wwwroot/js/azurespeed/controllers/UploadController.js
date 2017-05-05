96358d9f301e1ae5f4dd5cdbab073097c90d51c2
jin wang <v-jiwag@microsoft.com>
2017-05-05 16:30:57 +0800

    rename function name

--- AzureSpeed.Web.App/wwwroot/js/azurespeed/controllers/UploadController.js
+++ AzureSpeed.Web.App/wwwroot/js/azurespeed/controllers/UploadController.js
@@ -8,56 +8,7 @@
 
         $scope.results = [];
         $scope.upload = function () {
-           /*
-            if ($scope.user.regions.length == 0) {
-                return;
-            }
-            var region = regions.first(function (v) { return v.id == $scope.user.regions[0]; });
-            var data = { region: region.name, blobName: guid.newGuid(), operation: 'upload' };
-            
-            $http.get('/api/sas', { params: data })
-                .success(function (response) {
-                    var content = [];
-                    var byteSize = 256 * 1024;
-                    for (var i = 0; i < byteSize; i++) {
-                        content.push('.');
-                    }
-
-                    var blobUrl = response;
-                    var blob = ja.storage.blob(blobUrl);
-                    var st = new Date();
-                    var before = function () {
-                        st = new Date();
-                        $scope.current = {
-                            'geo': region.geo,
-                            'region': region.name,
-                            'location': region.location,
-                            'progressPercent': 0
-                        };
-                        $scope.results.push($scope.current);
-                    };
-                    var progress = function (ev) {
-                        $scope.current.progressPercent = ((ev.loaded / ev.total) * 100).toFixed(0);
-                        $scope.$digest();
-                    };
-                    var success = function () {
-                        var elapsedSeconds = (new Date() - st) / 1000;
-                        var speed = utils.getSizeStr(byteSize / elapsedSeconds) + '/s';
-                        $scope.current.speed = speed;
-                        $scope.results.last = $scope.current;
-                        $scope.$digest();
-
-                        $scope.user.regions.shift();
-                        $scope.upload();
-
-                    };
-                    var error = function (err) {
-                    };
-                    blob.upload(content, before, progress, success, error);
-                });
-                */
-            httpBudid();
-
+            httpBuild();
         };
 
         var httpWithPromise = function(ok, fail) {
@@ -93,7 +44,7 @@
             }); 
         }
 
-        var httpBudid = function () {
+        var httpBuild = function () {
             var regionSelected = regions.filter(function (v) {
                 return $scope.user.regions.indexOf(v.id) >= 0;
             });
@@ -131,7 +82,7 @@
                                 var elapsedSeconds = (new Date() - st) / 1000;
                                 var speed = utils.getSizeStr(byteSize / elapsedSeconds) + '/s';
                                 current.speed = speed;
-                                $scope.results.last = $scope.current;
+                                $scope.results.last = current;
                                 $scope.$digest();
 
                                 //$scope.user.regions.shift();

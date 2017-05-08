0e321ac827889d95d5141366c637d5d8a8589f24
jin wang <v-jiwag@microsoft.com>
2017-05-08 18:10:21 +0800

    Rewrite upload code with promises (#34)
    Rewrite upload code with promises

--- AzureSpeed.Web.App/wwwroot/js/azurespeed/controllers/UploadController.js
+++ AzureSpeed.Web.App/wwwroot/js/azurespeed/controllers/UploadController.js
@@ -7,56 +7,68 @@
         });
 
         $scope.results = [];
-        $scope.upload = function () {
-            if ($scope.user.regions.length == 0) {
-                return;
-            }
-            var region = regions.first(function (v) { return v.id == $scope.user.regions[0]; });
-            var data = { region: region.name, blobName: guid.newGuid(), operation: 'upload' };
-            $http.get('/api/sas', { params: data })
-                .then(function (response) {
-                    var content = [];
-                    var byteSize = 256 * 1024;
-                    for (var i = 0; i < byteSize; i++) {
-                        content.push('.');
-                    }
-
-                    var blobUrl = response.data;
-                    var blob = ja.storage.blob(blobUrl);
-                    var st = new Date();
-                    var before = function () {
-                        st = new Date();
-                        $scope.current = {
-                            'geo': region.geo,
-                            'region': region.name,
-                            'location': region.location,
-                            'progressPercent': 0
-                        };
-                        $scope.results.push($scope.current);
-                    };
-                    var progress = function (ev) {
-                        $scope.current.progressPercent = ((ev.loaded / ev.total) * 100).toFixed(0);
-                        $scope.$digest();
-                    };
-                    var success = function () {
-                        var elapsedSeconds = (new Date() - st) / 1000;
-                        var speed = utils.getSizeStr(byteSize / elapsedSeconds) + '/s';
-                        $scope.current.speed = speed;
-                        $scope.results.last = $scope.current;
-                        $scope.$digest();
-                        $scope.user.regions.shift();
-                        $scope.upload();
-                    };
-                    var error = function (err) {
-                    };
-                    blob.upload(content, before, progress, success, error);
-                }, function () {
-                });
-        };
         $scope.uploadLoop = function () {
-            $scope.upload();
-        };
+            var regionsSelected = regions.filter(function (v) {
+                return $scope.user.regions.indexOf(v.id) >= 0;
+            });
+
+            var list = [].map.call(regionsSelected, (region) => {
+                var data = { region: region.name, blobName: guid.newGuid(), operation: 'upload' };
+                return TaskAsync.run((callback) => {
+
+                    TaskAsync.run((cb) => {
+                        $http.get('/api/sas', { params: data })
+                            .success(function (response) {
+                                cb(response);
+                            })
+                    }).wait((res) => {
+                        var content = [];
+                        var byteSize = 256 * 1024;
+                        for (var i = 0; i < byteSize; i++) {
+                            content.push('.');
+                        }
+
+                        var blobUrl = res;
+                        var blob = ja.storage.blob(blobUrl);
+                        var st = new Date();
+                        var current = null;
+                        var before = function () {
+                            st = new Date();
+                            current = {
+                                'geo': region.geo,
+                                'region': region.name,
+                                'location': region.location,
+                                'progressPercent': 0
+                            };
+                            $scope.results.push(current);
+                        };
+                        var progress = function (ev) {
+                            current.progressPercent = ((ev.loaded / ev.total) * 100).toFixed(0);
+                            $scope.$digest();
+                        };
+                        var success = function () {
+                            var elapsedSeconds = (new Date() - st) / 1000;
+                            var speed = utils.getSizeStr(byteSize / elapsedSeconds) + '/s';
+                            current.speed = speed;
+                            $scope.results.last = current;
+                            $scope.$digest();
+                            callback("over!");
+                        };
+                        var error = function (err) {
+                        };
+                        blob.upload(content, before, progress, success, error);
+                    });
+                });
+            });
+
+            TaskAsync
+                .whenAll(list)
+                .wait((sucess) => { console.log(sucess); });
+        }
+
         $scope.canClick = function () {
             return true;
         }
+
     }]);
+

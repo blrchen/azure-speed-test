64864222ca3e5a4b509116f569bff62913dd591b
blchen <blrchen@hotmail.com>
2017-05-28 17:55:07 +0800

    Add azure env and region pages

--- AzureSpeed.Web.App/Controllers/InformationController.cs
+++ AzureSpeed.Web.App/Controllers/InformationController.cs
@@ -4,6 +4,16 @@ namespace AzureSpeed.Web.App.Controllers
 {
     public class InformationController : ControllerBase
     {
+        public IActionResult AzureEnvironments()
+        {
+            return View();
+        }
+
+        public IActionResult AzureRegions()
+        {
+            return View();
+        }
+
         public IActionResult IpRange()
         {
             return View();

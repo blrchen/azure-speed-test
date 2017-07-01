e60e698e57e26318edeb9a5ab81ac7d6d412634d
blchen <blrchen@hotmail.com>
2017-07-01 16:59:06 +0800

    Organized files under scripts folder

---
+++ deployment/ArmDeploy-CreateStagingWebsite.ps1
@@ -0,0 +1,8 @@
+Select-AzureRmSubscription -SubscriptionId "8eaf4bf3-36ac-44f2-8f0a-87fc37036d30"
+
+$DeploymentName="CreateStagingWebsite-"+(Get-Date -Format "yyyyMMdd-hhmmss")
+New-AzureRmResourceGroupDeployment -ResourceGroupName "AzureSpeed" `
+    -Name $DeploymentName `
+    -Mode Incremental `
+    -TemplateFile "CreateStagingWebsite-Template.json" `
+    -TemplateParameterFile "CreateStagingWebsite-TempalteParamters.json"
--- scripts/DeployWebApp.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-msbuild ..\AzureSpeed.sln /p:VisualStudioVersion=14.0 /p:DeployOnBuild=true /p:PublishProfile=azurespeed-wus-staging.pubxml /p:Configuration=Release

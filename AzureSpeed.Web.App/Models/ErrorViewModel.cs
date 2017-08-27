716a7a5f69178fdd760941d1656cf8546adfd180
blrchen <blrchen@users.noreply.github.com>
2017-08-27 20:34:28 +0800

    Upgraded to asp.net core 2.0 (#52)
    * Changed target framework from net461 to netcoreapp2.0
    
    * Updated NuGet to use unified AspNetCore.All package
    
    * Upgraded Newtonsoft.Json
    
    * Fixed deploy script not working

---
+++ AzureSpeed.Web.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureSpeed.Web.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- AzureSpeed.Web.App/Program.cs
+++ AzureSpeed.Web.App/Program.cs
@@ -1,20 +1,18 @@
-﻿namespace AzureSpeed.Web.App
-{
-    using System.IO;
-    using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
+namespace AzureSpeed.Web.App
+{
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }

a1542324e891f2bb0a0b5d69101ab57e3261c20c
blchen <blrchen@hotmail.com>
2017-08-28 16:08:54 +0800

    Converted AdminConsole and Test to target dotnet core 2.0

--- AzureSpeed.AdminConsole/Program.cs
+++ AzureSpeed.AdminConsole/Program.cs
@@ -1,22 +1,21 @@
-﻿namespace AzureSpeed.AdminConsole
+﻿using System;
+
+namespace AzureSpeed.AdminConsole
 {
-    using System;
-
-    // This tool setups everything needed for an Azure storage account to run speed test.
-    //
-    // It performs following steps
-    //   1. Enable CORS
-    //   2. Create a container azurespeed
-    //   3. Create a callback.js which is used by latency test
-    //   4. Upload a 100 mb dummy file for download speed test.
-    //
-    // Note: to run this tool, you need
-    //   1. Update setting.json to have correct storage name and key
-    //   2. Have 100MB.bin file in c:\DelMe
-
-    internal class Program
+    public class Program
     {
-        private static void Main()
+        // This tool setups everything needed for an Azure storage account to run speed test.
+        //
+        // It performs following steps
+        //   1. Enable CORS
+        //   2. Create a container azurespeed
+        //   3. Create a callback.js which is used by latency test
+        //   4. Upload a 100 mb dummy file for download speed test.
+        //
+        // Note: to run this tool, you need
+        //   1. Update settings.json to have correct storage name and key
+        //   2. Have 100MB.bin file in c:\DelMe
+        public static void Main(string[] args = null)
         {
             var storageAccountInitilizer = new StorageAccountInitilizer();
             storageAccountInitilizer.InitializeAsync().Wait();
@@ -24,3 +23,4 @@
         }
     }
 }
+
